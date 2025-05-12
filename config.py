f40b1c17b92ca6f9025ae8d7053683b0e075c1a8
Kevin Shilla <shillakevin@gmail.com>
2025-05-12 15:47:45 -0400

    Initial commit

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
 
 class Config:
-    raw_db_url = os.getenv("DATABASE_URL", "postgresql://u9o7tguuile3km:pdbc0711015539daf265db61a087a4fc6d094ef8f1db4b7a7b763f88062108034@c3cj4hehegopde.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d1ho4vshg30ue9")
+    raw_db_url = os.getenv(
+        "DATABASE_URL",
+        "postgresql://postgres:67b17b31-714c-8007-8980-f01cf26e8ab1@db.zqnombmgoaattmgpnrrx.supabase.co:5432/postgres"
+    )
 
     fixed_db_url = raw_db_url.replace("postgres://", "postgresql://", 1)
 
