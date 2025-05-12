aac94e50bc0193c880694798a6f94ef6b90f4999
Kevin Shilla <shillakevin@gmail.com>
2025-05-12 17:53:07 -0400

    config updates

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 import os
 
 class Config:
-    raw_db_url = os.getenv(
-        "DATABASE_URL",
-        "postgresql://postgres:67b17b31-714c-8007-8980-f01cf26e8ab1@db.zqnombmgoaattmgpnrrx.supabase.co:5432/postgres"
-    )
-
+    raw_db_url = os.getenv("DATABASE_URL")           # set on Render
     fixed_db_url = raw_db_url.replace("postgres://", "postgresql://", 1)
 
     SQLALCHEMY_DATABASE_URI = fixed_db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev-key")
